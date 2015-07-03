a8ac168d7cd06fbb7bbee598d71a9721427c1380
Daniel Schlaug <daniel.schlaug@gmail.com>
2015-07-03 22:55:04 +0900

    Unity chan controls in playground

--- vocamon/Assets/UnityChan/Scripts/UnityChanControlScriptWithRgidBody.cs
+++ vocamon/Assets/UnityChan/Scripts/UnityChanControlScriptWithRgidBody.cs
@@ -14,7 +14,7 @@ using System.Collections;
 public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 {
 
-	public float animSpeed = 1.5f;				// アニメーション再生速度設定
+	public float animationSpeed = 1.5f;				// アニメーション再生速度設定
 	public float lookSmoother = 3.0f;			// a smoothing setting for camera motion
 	public bool useCurves = true;				// Mecanimでカーブ調整を使うか設定する
 												// このスイッチが入っていないとカーブは使われない
@@ -62,7 +62,7 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 		// CapsuleColliderコンポーネントのHeight、Centerの初期値を保存する
 		orgColHight = col.height;
 		orgVectColCenter = col.center;
-}
+	}
 	
 	
 // 以下、メイン処理.リジッドボディと絡めるので、FixedUpdate内で処理を行う.
@@ -72,7 +72,7 @@ public class UnityChanControlScriptWithRgidBody : MonoBehaviour
 		float v = Input.GetAxis("Vertical");				// 入力デバイスの垂直軸をvで定義
 		anim.SetFloat("Speed", v);							// Animator側で設定している"Speed"パラメタにvを渡す
 		anim.SetFloat("Direction", h); 						// Animator側で設定している"Direction"パラメタにhを渡す
-		anim.speed = animSpeed;								// Animatorのモーション再生速度に animSpeedを設定する
+		anim.speed = animationSpeed;								// Animatorのモーション再生速度に animSpeedを設定する
 		currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// 参照用のステート変数にBase Layer (0)の現在のステートを設定する
 		rb.useGravity = true;//ジャンプ中に重力を切るので、それ以外は重力の影響を受けるようにする
 		
