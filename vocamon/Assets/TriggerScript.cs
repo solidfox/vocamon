d22f73be2739542fc2f568d05b341168305814be
boolafish <boolafish@linboandeMacBook-Pro.local>
2015-07-06 15:48:45 +0900

    collider check done

---
+++ vocamon/Assets/TriggerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Enter trigger XD ");
+	}
+
+	void OnTriggerStay(Collider other){
+		Debug.Log ("Trigger stay XD");
+	}
+
+	void OnTriggerExit(Collider other){
+		Debug.Log ("Trigger Exit XD");
+	}
+}
